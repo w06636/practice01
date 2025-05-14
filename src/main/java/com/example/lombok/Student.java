e633762eacf4958c1089fb0fbfa75322ff6d1f1f
w06636 <willie06636@gmail.com>
2025-05-14 16:33:21 +0800

    Initial commit

---
+++ src/main/java/com/example/lombok/Student.java
@@ -0,0 +1,14 @@
+package com.example.lombok;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+@Data
+public class Student {
+
+    Integer id;
+    String name;
+}
---
+++ src/main/java/com/example/lombok/StudentController.java
@@ -0,0 +1,18 @@
+package com.example.lombok;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/test")
+    public String test() {
+
+        Student student = new Student();
+        student.setId(1);
+        student.setName("123");
+
+        return student.getName();
+    }
+}
